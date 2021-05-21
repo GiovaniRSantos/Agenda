f0994ae986f2bec0e6b6154cbaf15e4bed6cd758
kali <kali@kali.kali>
2021-05-21 20:11:27 -0300

    Tuplas e Listas

---
+++ lista.py
@@ -0,0 +1,15 @@
+nome = "Giovani"
+idade = 20
+
+minha_lista = ["azul","vermelho","amarelo", nome, idade]
+
+#append adiciona um elemento na lista
+
+minha_lista.append("exemploAppend")
+
+#opcao de remocao do elemento na lista
+
+minha_lista.remove("azul")
+
+for dados in minha_lista:
+    print(dados)
---
+++ tupla.py
@@ -0,0 +1,13 @@
+tupla_cores = ("amarelo", "azul", "roxo")
+lista_cores = ["amarelo", "azul", "roxo"]
+
+#Tambem é possivel usar as tuplas como as listas
+for cor in tupla_cores:
+    print(cor)
+#tuplas sao estaticas, nao é possivel inserir ou remover itens, as listas podem.
+
+#na tupla nao é possivel substituir o elemento por outro como o exemplo abaixo
+# lista_cores[1] = "branco"
+#
+# print(lista_cores)
+# print(tupla_cores)
