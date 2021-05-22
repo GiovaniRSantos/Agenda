5390cc6128471662fd1a71a83fad78c75fcc70ba
kali <kali@kali.kali>
2021-05-22 11:47:02 -0300

    refaturacao do code

--- agenda.py
+++ agenda.py
@@ -1,16 +1,25 @@
-AGENDA = {}
-
-AGENDA['Giovani'] = {"telefone": "11928198218",
+AGENDA = {'Giovani': {"telefone": "11928198218",
         "email": "teste@giovani.com",
-        "endereco": "avenida teste 1",}
+        "endereco": "avenida teste 1",},
+
+        'Maria': {"telefone": "1198123723",
+        "email": "teste@maria.com",
+        "endereco": "avenida teste 2",},
+}
+
 
 
 def mostrar_contatos():
     for contato in AGENDA:
-        print('Nome:', contato)
-        print('telefone:',AGENDA[contato]['telefone'])
-        print('email:',AGENDA[contato]['email'])
-        print('endereco:',AGENDA[contato]['endereco'])
+        buscar_contatos(contato)
+        print('--------------------')
 
 
-mostrar_contatos()
+def buscar_contatos(contato):
+    print('Nome:', contato)
+    print('telefone:', AGENDA[contato]['telefone'])
+    print('email:', AGENDA[contato]['email'])
+    print('endereco:', AGENDA[contato]['endereco'])
+
+
+buscar_contatos('Giovani')
